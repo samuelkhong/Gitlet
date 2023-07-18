ce256525af0ba575a830ab1a43d151b579c055e1
samuel K <samuelkarsengkhong@gmail.com>
2023-07-18 15:35:15 -0700

    Added init() to repository

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,8 @@
 package gitlet;
 
 import java.io.File;
+import java.util.HashMap;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -26,24 +28,43 @@ public class Repository {
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     public static final File OBJECTS_DIR = join(GITLET_DIR, "objects");
-    public static final File DIR_DIR = join(GITLET_DIR, "dir");
-    public static final File HEAD = join(GITLET_DIR, "HEAD");
+    public static final File REF_DIR = join(GITLET_DIR, "dir");
+    public static final File HEAD_FILE = join(GITLET_DIR, "HEAD");
     public static final File index = join(GITLET_DIR, "index");
 
     public static final File BLOBS_DIR = join(OBJECTS_DIR, "blobs");
     public static final File COMMIT_DIR = join(OBJECTS_DIR, "commits");
-    public static final File BLOBS_DIR = join(OBJECTS_DIR, "commitTree");
+
+    public static final File MASTER_DIR = join(REF_DIR, "master");
+
+    private String HEAD_PTR;
 
 
     /* TODO: fill in the rest of this class. */
 
+    /* checks if ./gitlet exists. If not, creates ./gitlet and all subdirectories.*/
     public void init() {
         if (!GITLET_DIR.exists()) {
             GITLET_DIR.mkdir();
-        }
-        if (!OBJECTS_DIR.exists()) {
             OBJECTS_DIR.mkdir();
+            REF_DIR.mkdir();
+            BLOBS_DIR.mkdir();
+            COMMIT_DIR.mkdir();
+            MASTER_DIR.mkdir();
+
+            // set head to master branch
+            HEAD_PTR = MASTER.getPath();
+            // save the HEAD path as a file.
+            saveHead();
         }
+
+
+    }
+
+
+    private void saveHead() {
+        Utils.writeObject(HEAD_FILE, HEAD_PTR);
+
 
 
     }
